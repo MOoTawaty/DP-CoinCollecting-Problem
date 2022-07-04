a98fa352a12103f10076d49ebda1e34fbcaeabc9
Mohamed Khaled <m.tawaty96@gmail.com>
2022-07-04 03:31:48 +0200

    Coin Collection algorithm

--- main.py
+++ main.py
@@ -7,18 +7,16 @@ class CoinCollecting:
     def __init__(self, row, column):
         self.row = row
         self.column = column
-        self.gameBoard = [[0] * self.column for i in range(self.row)]     #[ [None] * 5 for i1 in range(6) ]
-        print(self.gameBoard)
+        self.gameBoard = [[0] * self.column for i in range(self.row)]  # [ [None] * 5 for i1 in range(6) ]
+        # print(self.gameBoard)
 
     def initialize_TheBoard(self):
-        random_List = random.sample(range(1, 31), 15)
+        random_List = random.sample(range(1, 31), 9)
         # The relationship between number of matrix cell and its row & column
         for i in random_List:
-            ch = int(i / self.column)  # 5
+            row = int(i / self.column)  # 5
             if i % self.column == 0:
-                row = ch - 1
-            else:
-                row = ch
+                row = row - 1
             column = i - row * self.column - 1
             self.gameBoard[row][column] = 1
 
@@ -29,9 +27,47 @@ class CoinCollecting:
         print(self.gameBoard[4])
         print("Random list:", random_List)
 
+    def trying(self):
+        self.gameBoard[0][4] = 1
+        self.gameBoard[1][1] = 1
+        self.gameBoard[1][3] = 1
+        self.gameBoard[2][3] = 1
+        self.gameBoard[2][5] = 1
+        self.gameBoard[3][2] = 1
+        self.gameBoard[3][5] = 1
+        self.gameBoard[4][0] = 1
+        self.gameBoard[4][4] = 1
+
+        print(self.gameBoard[0])
+        print(self.gameBoard[1])
+        print(self.gameBoard[2])
+        print(self.gameBoard[3])
+        print(self.gameBoard[4])
+
+
+    def game_algorithm(self):
+        board_copy = []
+        board_copy = self.gameBoard
+
+        for i in self.row:  # 5 rows
+            for j in self.column:  # 6 column
+                if i == 0 and j == 0:
+                    board_copy[i][j] = 0 + board_copy[i][j]
+                elif i == 0:
+                    board_copy[i][j] = max(0, board_copy[i][j - 1]) + board_copy[i][j]
+                elif j == 0:
+                    board_copy[i][j] = max(board_copy[i - 1][j], 0) + board_copy[i][j]
+                else:
+                    board_copy[i][j] = max(board_copy[i - 1][j], board_copy[i][j - 1])+board_copy[i][j]
+
+        return board_copy[self.row-1][self.column-1]
+
+
 
 player = CoinCollecting(5, 6)
-player.initialize_TheBoard()
+# player.initialize_TheBoard()
+player.trying()
+player.game_algorithm()
 
 # Press the green button in the gutter to run the script.
 # if __name__ == '__main__':
