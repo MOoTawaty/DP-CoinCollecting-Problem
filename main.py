37d419d6cde364779182a8839394b77c59ec92cc
Mohamed Khaled <m.tawaty96@gmail.com>
2022-07-06 00:44:29 +0200

    finishing the problem algorith

--- main.py
+++ main.py
@@ -38,20 +38,31 @@ class CoinCollecting:
         self.gameBoard[4][0] = 1
         self.gameBoard[4][4] = 1
 
-        print(self.gameBoard[0])
-        print(self.gameBoard[1])
-        print(self.gameBoard[2])
-        print(self.gameBoard[3])
-        print(self.gameBoard[4])
+        for i in range(self.row):  # 5 rows
+            print(self.gameBoard[i])
+        print('\n')
 
+    def game_algorithm(self):
+        board_copy = self.gameBoard
+        for i in range(self.row):  # 5 rows
+            for j in range(self.column):  # 6 column
+                if i == 0 and j == 0:
+                    board_copy[i][j] = 0 + board_copy[i][j]
+                elif i == 0:
+                    board_copy[i][j] = max(0, board_copy[i][j - 1]) + board_copy[i][j]
+                elif j == 0:
+                    board_copy[i][j] = max(board_copy[i - 1][j], 0) + board_copy[i][j]
+                else:
+                    board_copy[i][j] = max(board_copy[i - 1][j], board_copy[i][j - 1]) + board_copy[i][j]
+            print(board_copy[i])
 
-
+        return board_copy[self.row - 1][self.column - 1]
 
 
 player = CoinCollecting(5, 6)
 # player.initialize_TheBoard()
 player.trying()
-#player.game_algorithm()
+print(player.game_algorithm())
 
 # Press the green button in the gutter to run the script.
 # if __name__ == '__main__':
