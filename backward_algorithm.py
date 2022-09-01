ba9c844bb04fabfc77dd71bc5f1d2198f256a568
Mohamed Khaled <m.tawaty96@gmail.com>
2022-09-01 21:12:31 +0200

    finishing the second part of the algorithm

---
+++ backward_algorithm.py
@@ -0,0 +1,28 @@
+import game_algorithm
+
+
+class BackwardGameAlgorithm(game_algorithm.Game):
+
+    def backward_game_algorithm(self):
+
+        # to accessing the values in the board in reverse mode
+        row = self.row - 1  # 4
+        column = self.column - 1  # 5
+
+        # playing the first and last values in the board
+        self.gameBoard[row][column] = 9
+        self.gameBoard[0][0] = 9
+
+        # finish when i=1 as first rew already assigned
+        while row >= 1:
+            if self.gameBoard[row][column - 1] > self.gameBoard[row - 1][column]:  # leftward
+                # moving to the left
+                self.gameBoard[row][column - 1] = 9
+                column = column - 1
+            else:
+                # moving to the up
+                self.gameBoard[row - 1][column] = 9  # upward
+                row = row - 1
+
+        for row in range(self.row):
+            print(self.gameBoard[row])
