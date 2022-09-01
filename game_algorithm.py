38ec33a66dd9e7a1fd5638312a7cdf5f6b51a528
Mohamed Khaled <m.tawaty96@gmail.com>
2022-09-01 21:10:12 +0200

    refactoring the game algorithm to support separation of concerns

---
+++ game_algorithm.py
@@ -0,0 +1,8 @@
+import coin_collecting
+
+
+class Game(coin_collecting.CoinCollecting):
+
+    # demo function to check the algorithm
+    def demo_data(self, demo_board):
+        self.gameBoard = demo_board
