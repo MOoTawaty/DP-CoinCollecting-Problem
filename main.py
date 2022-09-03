23da77356d8c6d02d014f813a0253b01ad7e417a
Mohamed Khaled <m.tawaty96@gmail.com>
2022-09-03 17:55:55 +0200

    initialize the board cells randomly

--- main.py
+++ main.py
@@ -4,27 +4,31 @@ import forward_algorithm
 import game_algorithm
 
 
-
 # the main function of the system.
 if __name__ == '__main__':
-    # initializing player data "empty board"
-    player = game_algorithm.Game(5, 6)
+    # create empty board row*column
+    row, column = 6, 6
+    player = game_algorithm.Game(row, column)
 
-    # initialize the demo board to the game
-    demo = demo_example.Demo(player.gameBoard)
-    gameBoard = demo.demo()
+    # initialize the board cells randomly
+    player.initialize_TheBoard()
 
-    # sending demo board to be processed
-    player.demo_data(gameBoard)
+    # # this is a demo example to check the algorithm with row*col 5*6
+    # # apply the demo board to the game
+    # demo = demo_example.Demo(player.gameBoard)
+    # gameBoard = demo.demo()
+    # # sending demo board to be processed
+    # player.demo_data(gameBoard)
 
-    # forward move
+    # getting maximum Coins Picked
     f_algo = forward_algorithm.ForwardGameAlgorithm
-    f_algo.forward_game_algorithm(player)
+    maxCoinPicked = f_algo.max_coin_picked(player)
+    print("maximum Coins Picked: ", maxCoinPicked, '\n')
 
-    print()
-
-    # backward move
+    # getting path in stars
+    print("path in stars: ")
     b_algo = backward_algorithm.BackwardGameAlgorithm
-    b_algo.backward_game_algorithm(player)
+    b_algo.path(player)
 
-    # player.initialize_TheBoard()
+
+
